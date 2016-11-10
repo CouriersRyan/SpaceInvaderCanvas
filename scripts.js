9e8222da065841afe4cd56446bf01329bf8fc85a
CouriersRyan <zhangryan09@gmail.com>
2016-11-09 17:03:50 -0800

    adds the player

--- scripts.js
+++ scripts.js
@@ -4,3 +4,29 @@ var CANVAS_HEIGHT = 320;
 var canvasElement = $("<canvas width ='" + CANVAS_WIDTH + "' height='" + CANVAS_HEIGHT +"'></canvas>");
 var canvas = canvasElement.get(0).getContext("2d");
 canvasElement.appendTo('body');
+
+var FPS = 30;
+setInterval(function(){
+	update();
+	draw();
+}, 1000/FPS);
+
+function update(){
+};
+
+function draw(){
+	canvas.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+	player.draw()
+};
+
+var player = {
+	color: "#00a",
+	x: 220,
+	y: 270,
+	width: 32,
+	height: 32,
+	draw: function(){
+		canvas.fillStyle = this.color;
+		canvas.fillRect(this.x, this.y, this.width, this.height);
+	}
+};
